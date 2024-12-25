1357ac5e236a4b54c34bd92067c1b66232210705
MonkGirl <monkgirl2014@gmail.com>
2024-12-25 15:24:52 +0800

    feat(*): ci project test
    qodana teamcity ci test

---
+++ src/main/java/com/monkgirl/testproject/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.monkgirl.testproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author MissYoung
+ * @version 0.1
+ * @since 2024-12-25
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello, World!";
+    }
+}
